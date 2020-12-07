268f1ab3bf4c28250c0b07bffb53840c4f8a4e91
Dmitry DM <ultrwave@gmail.com>
2020-12-07 20:23:53 +0300

    Navbar > friends online preview

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,26 +1,56 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import Style from './Navbar.module.css';
 
+type NavbarItemPropsType = {
+    to: string
+    title: string
+}
 
 export function Navbar() {
+
+    function NavbarItem(props: NavbarItemPropsType) {
+        return (
+            <div className={Style.item}>
+                <NavLink to={props.to}
+                         activeClassName={Style.activeLink}>
+                    {props.title}
+                </NavLink>
+            </div>
+        )
+    }
+
+    function FriendsOnline() {
+        return (
+            <div>
+                <h2 className={Style.friendsTitle}>Friends online</h2>
+                <div className={Style.friendsOnline}>
+                    <div className={Style.friend}>
+                        <div className={Style.friendAvatar}></div>
+                        <span className={Style.friendName}>Joe</span>
+                    </div>
+                    <div className={Style.friend}>
+                        <div className={Style.friendAvatar}></div>
+                        <span className={Style.friendName}>Jack</span>
+                    </div>
+                    <div className={Style.friend}>
+                        <div className={Style.friendAvatar}></div>
+                        <span className={Style.friendName}>Jane</span>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
     return (
         <nav className={Style.navbar}>
-            <div className={Style.item}>
-                <NavLink to="/profile" activeClassName={Style.activeLink}>Profile</NavLink>
+            <div className={Style.navbarItems}>
+                <NavbarItem to='/profile' title='Profile'/>
+                <NavbarItem to='/dialogs' title='Dialogs'/>
+                <NavbarItem to='/news' title='News'/>
+                <NavbarItem to='/settings' title='Settings'/>
             </div>
-            <div className={Style.item}>
-                <NavLink to="/dialogs" activeClassName={Style.activeLink}>Messages</NavLink>
-            </div>
-            <div className={Style.item}>
-                <NavLink to="/news" activeClassName={Style.activeLink}>News</NavLink>
-            </div>
-            <div className={Style.item}>
-                <NavLink to="/music" activeClassName={Style.activeLink}>Music</NavLink>
-            </div>
-            <div className={Style.item}>
-                <NavLink to="/settings" activeClassName={Style.activeLink}>Settings</NavLink>
-            </div>
+            <FriendsOnline/>
         </nav>
     )
 }
