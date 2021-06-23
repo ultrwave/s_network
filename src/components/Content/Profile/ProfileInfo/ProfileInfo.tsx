0fadba79e015add57612e4436693ffdaea4b15eb
Dmitry DM <ultrwave@gmail.com>
2021-06-23 23:40:09 +0300

    ProfileInfo > toggleEditMode

--- src/components/Content/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Content/Profile/ProfileInfo/ProfileInfo.tsx
@@ -22,6 +22,8 @@ export function ProfileInfo(props: ProfileInfoProps) {
 
     let [editMode, setEditMode] = useState(false)
 
+    let toggleEditMode = () => setEditMode(!editMode)
+
     if (!props.profile) {
         return <Preloader/>
     }
@@ -59,7 +61,7 @@ export function ProfileInfo(props: ProfileInfoProps) {
                                  contacts={contacts}
                                  updateUserStatus={props.updateUserStatus}
                                  onMainPhotoSelected={onMainPhotoSelected}
-                                 goToEditMode={() => {}}
+                                 goToEditMode={toggleEditMode}
                     />
                     : <ProfileDataForm profile={props.profile} />
                 }
