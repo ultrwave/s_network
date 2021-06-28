e885a0b3741f1daca2b786a4ebd4810b9b424fab
Dmitry DM <ultrwave@gmail.com>
2021-06-28 23:58:41 +0300

    FormsControls > createNewField

--- src/components/Content/Profile/ProfileInfo/ProfileDataForm.tsx
+++ src/components/Content/Profile/ProfileInfo/ProfileDataForm.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import {UserProfileType} from '../../../../types/types';
+import Style from '../Profile.module.css';
+import {ProfileStatusWithHooks} from './ProfileStatusWithHooks';
 
 type ProfileDataFormType = {
     profile: UserProfileType
 }
 
 export const ProfileDataForm = (profile: ProfileDataFormType) => {
-    return <div>
-        Form
-    </div>
+    return (
+        <form className={Style.profileInfo}>
+            <button onClick={props.setEditModeOff}>save changes</button>
+            <div className={Style.fullName}>{props.profile.fullName}</div>
+            <div className={Style.aboutMe}>{props.profile.aboutMe}</div>
+            <div className={Style.lookingForAJob}>{props.profile.lookingForAJob}</div>
+            <div className={Style.lookingForAJob}>{props.profile.lookingForAJobDescription}</div>
+            <ProfileStatusWithHooks
+                status={props.status}
+                updateUserStatus={props.updateUserStatus}
+            />
+            <div className={Style.contacts}>Contacts:
+                {props.contacts}
+            </div>
+            {props.isOwner && <input type="file" onChange={props.onMainPhotoSelected}/>}
+        </form>
+    )
 }
+
