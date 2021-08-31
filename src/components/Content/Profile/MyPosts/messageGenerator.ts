82b963640a233b8941fa4ece334cd149474abc5d
Dmitry DM <ultrwave@gmail.com>
2021-08-31 22:19:53 +0300

    MyPosts > messageGenerator

---
+++ src/components/Content/Profile/MyPosts/messageGenerator.ts
@@ -0,0 +1,16 @@
+
+const messageParts = {
+    beginnings: ['Hi! ', 'Hello! ', 'What a good day...', 'What\'s up everybody? ', 'Hello once again! '],
+    bodies: ['It\'s time for a new post! ', 'I wish you all productive day! ', 'How are you today? '],
+    endings: ['React', 'Redux', 'TypeScript']
+}
+
+export function generateMessage(name: string) {
+    const beginning = messageParts.beginnings[Math.floor(messageParts.beginnings.length * Math.random())]
+    const body = messageParts.bodies[Math.floor(messageParts.bodies.length * Math.random())]
+    const ending = messageParts.endings[Math.floor(messageParts.endings.length * Math.random())]
+    return (beginning
+        + (Math.round(Math.random()) ? name + ' here!' : '')
+        + body
+        + (Math.round(Math.random()) ? 'Today, i am going to learn ' + ending : ''))
+}
