ad7c9d4579e74815ac021e9b2ee85c2043689451
Dmitry DM <ultrwave@gmail.com>
2021-09-14 00:03:32 +0300

    FriendsOnline > loading animation when switching display mode

--- src/components/Navbar/FriendsOnline/FriendsOnline.tsx
+++ src/components/Navbar/FriendsOnline/FriendsOnline.tsx
@@ -1,14 +1,14 @@
 import Style from '../Navbar.module.css';
 import React, {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
-import {getFriendsOnlineThunk} from '../../../redux/users-reducer';
+import {getFriendsOnlineThunk, setFriendsOnline} from '../../../redux/users-reducer';
 import {StateType, UserType} from '../../../types/types';
 import {Friend} from './Friend';
 
 export function FriendsOnline() {
 
     const dispatch = useDispatch()
-    let [latestFriends, toggleLatestFriends] = useState(false)
+    let [latestFriends, setLatestFriends] = useState(false)
 
     useEffect(() => {
         dispatch(getFriendsOnlineThunk(latestFriends))
@@ -17,15 +17,22 @@ export function FriendsOnline() {
     let friends: Array<UserType | undefined> = useSelector(
         (state: StateType) => state.pageUsers.friendsOnline)
 
+    let friendsLoading = (new Array(3)).fill(undefined)
+
     if (!friends.length) {
-        friends = (new Array(3)).fill(undefined)
+        friends = friendsLoading
+    }
+
+    const toggleLatestFriends = () => {
+        dispatch(setFriendsOnline({friendsOnline: friendsLoading}))
+        setLatestFriends(!latestFriends)
     }
 
     return ( // todo - add refresh icon
         <div>
             <h2 className={Style.friendsTitle}
                 onClick={() => dispatch(getFriendsOnlineThunk(latestFriends))}>Friends online</h2>
-            <span onClick={() => toggleLatestFriends(!latestFriends)}>
+            <span onClick={toggleLatestFriends}>
                 {'Showing ' + (latestFriends ? 'latest' : 'random')}</span>
             <div className={Style.friendsOnline}>
                 {friends.map((f, i) => <Friend friend={f} key={(f && f.id) || i}/>)}
